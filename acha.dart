4cd0901558a568603b08aa56b983c511d7d54586
Hedmon Achacha <hedmonachacha@gmail.com>
2023-08-31 21:23:04 +0300

    here is my assignment Sir

---
+++ acha.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "My name is Hedmon Onyango Achacha ,";
+  var school = "a student at PLP Compassion Kenya Cohort 2 .";
+  var city = "I come from Kisumu city ,";
+  var country = "Kenya.";
+  print(name + school + city + country);
+}
